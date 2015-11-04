4edd50d61bbce019b66d01ca5e052d93ced319da
Mingcong Bai <jeffbai@aosc.xyz>
2015-11-04 02:56:17 +0000

    firefox: update to 42.0
    From updated vendor.js...
    - accessibility enhancements
    - HiDPI support enhancements
    - Firefox will no longer remind user about not being the deafult choice of web browser

--- extra-web/firefox/autobuild/overrides/usr/lib/firefox/browser/defaults/preferences/vendor.js
+++ extra-web/firefox/autobuild/overrides/usr/lib/firefox/browser/defaults/preferences/vendor.js
@@ -1,10 +1,26 @@
-// Use LANG environment variable to choose locale
-pref("intl.locale.matchOS", true);
+// Use LANG environment variable to choose locale.
+pref("intl.locale.matchOS",			true);
 
 // Disable default browser checking.
-pref("browser.shell.checkDefaultBrowser", false);
+pref("browser.shell.checkDefaultBrowser",	false);
 
-// Don't disable our bundled extensions in the application directory
-pref("extensions.autoDisableScopes", 11);
-pref("extensions.shownSelectionUI", true);
+// Don't disable our bundled extensions in the application directory.
+pref("extensions.autoDisableScopes",		11);
+pref("extensions.shownSelectionUI",		true);
 
+// ... from Gentoo ...
+
+// Use system defined typography colors.
+pref("browser.display.use_system_colors",	true);
+
+// Backspace defined to go back a page in session history.
+pref("browser.backspace_action",		0);
+
+// Enable find/search as type begins.
+pref("accessibility.typeaheadfind",		true);
+
+// Do not bother those who wouldn't want Firefox as default anyways.
+pref("browser.shell.checkDefaultBrowser",	false);
+
+// Always use system defined logical resolution for CSS DPI detection.
+pref("layout.css.dpi",				0);
