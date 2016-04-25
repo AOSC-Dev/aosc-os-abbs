98cfee5f1cc2bdc213aa78b4b2cc8848e1c115d8
Mingcong Bai <jeffbai@aosc.xyz>
2016-04-24 21:21:59 -0600

    firefox: use GL layer acceleration [pending]

--- extra-web/firefox/autobuild/overrides/usr/lib/firefox/browser/defaults/preferences/vendor.js
+++ extra-web/firefox/autobuild/overrides/usr/lib/firefox/browser/defaults/preferences/vendor.js
@@ -24,3 +24,6 @@ pref("browser.shell.checkDefaultBrowser",	false);
 
 // Always use system defined logical resolution for CSS DPI detection.
 pref("layout.css.dpi",				0);
+
+// Enable GL layers acceleration.
+pref("layers.acceleration.force-enabled",	true);
