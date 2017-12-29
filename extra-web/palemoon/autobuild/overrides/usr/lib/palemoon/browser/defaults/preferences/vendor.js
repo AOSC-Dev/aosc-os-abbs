359f565a977bbc1ff0646782064c46340624981e
Mingcong Bai <jeffbai@aosc.xyz>
2017-12-29 03:01:20 +0000

    palemoon: attempt 1/n to comply with upstream licensing restrictions

--- extra-web/palemoon/autobuild/overrides/usr/lib/palemoon/browser/defaults/preferences/vendor.js
+++ extra-web/palemoon/autobuild/overrides/usr/lib/palemoon/browser/defaults/preferences/vendor.js
@@ -1,6 +1,2 @@
 // Use LANG environment variable to choose locale.
 pref("intl.locale.matchOS",			true);
-
-// Disable install warning for bundled extension in the application directory
-pref("extensions.autoDisableScopes", 11);
-pref("extensions.shownSelectionUI", true);
