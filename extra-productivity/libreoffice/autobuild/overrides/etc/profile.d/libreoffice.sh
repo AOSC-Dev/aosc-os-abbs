7469db2b1ede02caf1b3bd5325f8b40fc7845116
Mingcong Bai <jeffbai@aosc.xyz>
2018-02-14 03:49:28 +0000

    libreoffice: update to 6.0.1.1; #1011

---
+++ extra-productivity/libreoffice/autobuild/overrides/etc/profile.d/libreoffice.sh
@@ -0,0 +1,8 @@
+# Uncomment to force a certain visual style in LibreOffice...
+#     - gen: Generic/X11
+#     - gtk: GTK+ 2.0
+#     - gtk3: GTK+ 3.0
+
+#export SAL_USE_VCLPLUGIN=gen
+#export SAL_USE_VCLPLUGIN=gtk
+#export SAL_USE_VCLPLUGIN=gtk3
