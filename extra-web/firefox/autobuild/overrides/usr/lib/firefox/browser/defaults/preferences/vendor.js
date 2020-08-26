2a301db8733f7d409fdec20ba4a708cd431a9071
eatradish <sakiiily@aosc.io>
2020-08-26 15:31:54 +0800

    firefox: merge vendor.js

--- extra-web/firefox/autobuild/overrides/usr/lib/firefox/browser/defaults/preferences/vendor.js
+++ extra-web/firefox/autobuild/overrides/usr/lib/firefox/browser/defaults/preferences/vendor.js
@@ -23,6 +23,10 @@ pref("browser.shell.checkDefaultBrowser",	false);
 pref("layout.css.dpi",				0);
 
 // ... AOSC OS policies ...
+// Enable Hardware acceleration (WARNING: Check again after Firefox 81)
+perf("media.ffmpeg.dmabuf-textures.enabled",   true);
+perf("media.ffmpeg.vaapi-drm-display.enabled", true);
+perf("media.ffmpeg.vaapi.enabled",             true);
 
 // Disable Pocket by default.
 pref("browser.pocket.enabled",			false);
