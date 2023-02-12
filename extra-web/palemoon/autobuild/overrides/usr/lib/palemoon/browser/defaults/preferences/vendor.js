345a85ac10331db21a2a320c4af576529a553eb1
Mingcong Bai <jeffbai@aosc.xyz>
2023-02-11 22:56:14 -0800

    palemoon: drop, orphaned

--- extra-web/palemoon/autobuild/overrides/usr/lib/palemoon/browser/defaults/preferences/vendor.js
+++
@@ -1,6 +0,0 @@
-// Use LANG environment variable to choose locale.
-pref("intl.locale.matchOS",			true);
-
-// Arch Linux: Don't disable our bundled extensions in the application directory
-pref("extensions.autoDisableScopes",		11);
-pref("extensions.shownSelectionUI",		true);
