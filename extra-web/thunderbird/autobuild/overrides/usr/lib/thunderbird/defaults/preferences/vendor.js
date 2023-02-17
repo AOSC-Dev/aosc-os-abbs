0ab351b687c184ba1521970acd20c2233796376b
Mingcong Bai <jeffbai@aosc.xyz>
2023-02-16 21:55:22 -0800

    thunderbird: update to 102.7.2
    - Survey vendor.js
    - Update Sequoia to fix build with Rust 1.67.0.

--- extra-web/thunderbird/autobuild/overrides/usr/lib/thunderbird/defaults/preferences/vendor.js
+++ extra-web/thunderbird/autobuild/overrides/usr/lib/thunderbird/defaults/preferences/vendor.js
@@ -1,12 +1,30 @@
 // Use LANG environment variable to choose locale
-pref("intl.locale.requested", "");
+pref("intl.locale.requested",				"");
 
 // Use system-provided dictionaries
-pref("spellchecker.dictionary_path", "/usr/share/hunspell");
+pref("spellchecker.dictionary_path",			"/usr/share/hunspell");
 
 // Disable default mailer checking.
-pref("mail.shell.checkDefaultMail", false);
+pref("mail.shell.checkDefaultMail",			false);
 
 // Don't disable our bundled extensions in the application directory
-pref("extensions.autoDisableScopes", 11);
-pref("extensions.shownSelectionUI", true);
+pref("extensions.autoDisableScopes",			11);
+pref("extensions.shownSelectionUI",			true);
+
+// Let XDG Desktop Portal handle file pickers.
+// This allows Firefox to use native file dialogs (such as KDE).
+pref("widget.use-xdg-desktop-portal.file-picker",	1);
+
+// ... From Fedora ...
+
+// Disable auto update.
+pref("app.update.enabled",				false);
+pref("app.update.autoInstallEnabled",			false);
+
+// Detect offline status.
+pref("offline.autoDetect",				true);
+
+// Disable telemetry (AOSC OS policy).
+pref("datareporting.healthreport.uploadEnabled",	false);
+pref("datareporting.policy.dataSubmissionEnabled",	false);
+pref("toolkit.telemetry.archive.enabled",		false);
