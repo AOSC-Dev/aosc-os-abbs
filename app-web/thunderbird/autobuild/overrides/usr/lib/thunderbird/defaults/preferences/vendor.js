b57b90c9f0e6aae0419a4225c943afe03c73c489
Mingcong Bai <jeffbai@aosc.xyz>
2023-09-20 09:56:22 -0700

    thunderbird: respect system scaling
    Drop font size and layout overrides, as they should not have been specified.

--- app-web/thunderbird/autobuild/overrides/usr/lib/thunderbird/defaults/preferences/vendor.js
+++ app-web/thunderbird/autobuild/overrides/usr/lib/thunderbird/defaults/preferences/vendor.js
@@ -1,7 +1,7 @@
-// Use LANG environment variable to choose locale
+// Use LANG environment variable to choose locale.
 pref("intl.locale.requested",				"");
 
-// Use system-provided dictionaries
+// Use system-provided dictionaries.
 pref("spellchecker.dictionary_path",			"/usr/share/hunspell");
 
 // Disable default mailer checking.
@@ -18,6 +18,9 @@ pref("widget.use-xdg-desktop-portal.file-picker",	1);
 // Disable oft-broken client-side decoration.
 pref("mail.tabs.drawInTitlebar",			false);
 
+// Let the system handle DPI scaling.
+pref("layout.css.devPixelsPerPx",                       -1.0);
+
 // ... From Fedora ...
 
 // Disable auto update.
@@ -31,9 +34,3 @@ pref("offline.autoDetect",				true);
 pref("datareporting.healthreport.uploadEnabled",	false);
 pref("datareporting.policy.dataSubmissionEnabled",	false);
 pref("toolkit.telemetry.archive.enabled",		false);
-
-// Default to our default font size (10px).
-pref("mail.uifontsize",					10);
-
-// Default to the "compact" (0) UI density, as it seems more appropriate for desktop devices.
-pref("mail.uidensity",					0);
