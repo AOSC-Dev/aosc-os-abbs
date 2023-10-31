a5f689563dbb8c121eec0912a2ae7f82bf116c0c
Kay Lin <kaymw@aosc.io>
2023-10-30 20:16:49 -0700

    firefox: update to 119.0.1
    - Rebase LoongArch patch to Firefox 119.0.
    - Fix "unexpected character" in vendor.js.
    - Remove architectural limit on VA-API support.
    - Enable VA-API support on AMD GPUs.
    - Remove media.rdd-process.enabled=false.
    - Remove outdated VA-API-related prefs from vendor.js.
    - (Experimental) Set MOZ_USE_XINPUT2=1. The upstream claims the tooltip bug has
      been fixed in Firefox 119.
      Ref: https://bugzilla.mozilla.org/show_bug.cgi?id=148624

--- app-web/firefox/autobuild/overrides/etc/profile.d/firefox.sh
+++ app-web/firefox/autobuild/overrides/etc/profile.d/firefox.sh
@@ -1,6 +1,2 @@
 # Whether to use smooth scrolling by default.
-#
-# Disabled due to a tooltip display bug.
-#
-# Ref: https://bugzilla.mozilla.org/show_bug.cgi?id=1805703
-export MOZ_USE_XINPUT2=0
+export MOZ_USE_XINPUT2=1
