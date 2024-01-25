c8fed27bba03feea9796985e5d78604fd64ab6eb
Jiajie Chen <c@jia.je>
2024-01-25 08:40:39 -0800

    chromium: update to 121.0.6167.85
    - Add Debian patches
    - Update patches from Fedora
    - Add patch to fix clang 17 library path
    - Rustc is required to build chromium since 121
    - Import patches from AOSC-Dev/chromium-loongarch64

---
+++ app-web/chromium/autobuild/patches/copy.sh
@@ -0,0 +1,11 @@
+PREFIX=/buildroots/jiegec/build-chromium/chromium-loongarch64/chromium/chromium-121.0.6167.85
+cp $PREFIX.loongarch64-clang-no-lsx.diff 0040-chromium-loongarch64-clang-no-lsx.patch.loongarch64
+cp $PREFIX.loongarch64-crashpad.diff 0040-chromium-loongarch64-crashpad.patch.loongarch64
+cp $PREFIX.loongarch64-ffmpeg.diff 0040-chromium-loongarch64-ffmpeg.patch.loongarch64
+cp $PREFIX.loongarch64-sandbox.diff 0040-chromium-loongarch64-sandbox.patch.loongarch64
+cp $PREFIX.loongarch64-swiftshader.diff 0040-chromium-loongarch64-swiftshader.patch.loongarch64
+cp $PREFIX.loongarch64-dav1d.diff 0040-chromium-loongarch64-dav1d.patch.loongarch64
+cp $PREFIX.loongarch64-medium-cmodel.diff 0040-chromium-loongarch64-medium-cmodel.patch.loongarch64
+cp $PREFIX.loongarch64.diff 0040-chromium-loongarch64.patch.loongarch64
+cp $PREFIX.rollup.diff 0040-chromium-rollup.patch.loongarch64
+cp $PREFIX.rust-ld-bfd.diff 0040-chromium-rust-ld-bfd.patch.loongarch64
