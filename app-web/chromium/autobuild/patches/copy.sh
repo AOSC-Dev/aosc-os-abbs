8908eb4be0b7f0fa856e277f4261fabb89a7de45
Jiajie Chen <c@jia.je>
2024-04-21 19:22:55 -0700

    chromium: update to 124.0.6367.60

--- app-web/chromium/autobuild/patches/copy.sh
+++ app-web/chromium/autobuild/patches/copy.sh
@@ -1,5 +1,5 @@
 set -x
-VER=123.0.6312.86
+VER=124.0.6367.60
 PREFIX=/buildroots/jiegec/build-chromium/chromium-loongarch64/chromium/chromium-$VER
 cp $PREFIX.*.diff .
 rename -v chromium-$VER. "" *.diff
