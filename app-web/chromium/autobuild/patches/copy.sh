4b1d80634967edcfbf8aef83e280fc2918c4ccb0
Mingcong Bai <jeffbai@aosc.io>
2024-05-06 13:47:19 -0700

    chromium: update to 124.0.6367.118
    Improve copy.sh => copy-loongarch64.bash, which:
    
    - Sources current version from spec.
    - Clones, copies, and cleans up automatically.

--- app-web/chromium/autobuild/patches/copy.sh
+++
@@ -1,6 +0,0 @@
-set -x
-VER=124.0.6367.60
-PREFIX=/buildroots/jiegec/build-chromium/chromium-loongarch64/chromium/chromium-$VER
-cp $PREFIX.*.diff .
-rename -v chromium-$VER. "" *.diff
-rename -v .diff .patch *.diff
