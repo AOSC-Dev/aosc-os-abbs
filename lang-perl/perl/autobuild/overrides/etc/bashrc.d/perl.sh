130a6f9bc579370efb233e74100d26a47b57a47b
Mingcong Bai <jeffbai@aosc.io>
2024-08-07 00:33:04 +0800

    perl: append PATH unconditionally
    Previously, the profile script only appends Perl core, site, and vendor
    executable paths only if the paths exist. This can cause issues where a
    package creates one of those paths, as the user would not immediately be able
    to make use of the executables they installed (until the next login).
    
    Append PATHs unconditionally since it would not cause problems if any of the
    paths do not exist.

--- lang-perl/perl/autobuild/overrides/etc/bashrc.d/perl.sh
+++ lang-perl/perl/autobuild/overrides/etc/bashrc.d/perl.sh
@@ -1,5 +1,1 @@
-[ -d /usr/bin/site_perl ] && PATH="$PATH:/usr/bin/site_perl"
-[ -d /usr/lib/perl5/site_perl/bin ] && PATH="$PATH:/usr/lib/perl5/site_perl/bin"
-[ -d /usr/bin/vendor_perl ] && PATH="$PATH:/usr/bin/vendor_perl"
-[ -d /usr/lib/perl5/vendor_perl/bin ] && PATH="$PATH:/usr/lib/perl5/vendor_perl/bin"
-[ -d /usr/bin/core_perl ] && PATH="$PATH:/usr/bin/core_perl"
+PATH="$PATH:/usr/bin/site_perl:/usr/lib/perl5/site_perl/bin:/usr/bin/vendor_perl:/usr/lib/perl5/vendor_perl/bin:/usr/bin/core_perl:/usr/lib/perl5/core_perl/bin"
