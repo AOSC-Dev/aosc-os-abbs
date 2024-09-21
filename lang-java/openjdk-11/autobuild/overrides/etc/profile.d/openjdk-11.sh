2251a2ee5948cf592f6f62fdee7c3432563b9747
xtex <xtexchooser@duck.com>
2024-09-21 21:44:49 +0800

    openjdk-11: update to 11.0.24+ga

--- lang-java/openjdk-11/autobuild/overrides/etc/profile.d/openjdk-11.sh
+++
@@ -1,4 +0,0 @@
-# FIXME: This have been changed to use a patch in other JDK versions
-#        See commit 38a1699a47cd67139db41b7c1d559e345f153166 at github.com/AOSC-Tracking/jdk
-#        However the patch is not backported to JDK 11 yet.
-export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=on"
