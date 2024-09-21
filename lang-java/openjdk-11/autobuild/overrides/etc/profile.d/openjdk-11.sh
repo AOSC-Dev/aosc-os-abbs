b7a71babc94a6be7f4dc41ec1059529835893db1
xtex <xtexchooser@duck.com>
2024-09-21 21:00:11 +0800

    openjdk-11: migrate to use alternatives for JAVA_HOME

--- lang-java/openjdk-11/autobuild/overrides/etc/profile.d/openjdk-11.sh
+++ lang-java/openjdk-11/autobuild/overrides/etc/profile.d/openjdk-11.sh
@@ -1,3 +1,4 @@
-export JAVA_HOME="/usr/lib/java"
+# FIXME: This have been changed to use a patch in other JDK versions
+#        See commit 38a1699a47cd67139db41b7c1d559e345f153166 at github.com/AOSC-Tracking/jdk
+#        However the patch is not backported to JDK 11 yet.
 export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=on"
-export JAVA_FONTS="/usr/share/fonts"
