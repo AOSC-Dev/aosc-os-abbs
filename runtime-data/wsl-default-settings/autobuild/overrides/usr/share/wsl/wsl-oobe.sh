91683f2a4d3c27aeb903334886fec88599c416a2
Kexy Biscuit a.k.a. るる <kexybiscuit@outlook.com>
2025-03-19 19:03:44 +0800

    wsl-default-settings: prompt the user to change password until it succeeds

--- runtime-data/wsl-default-settings/autobuild/overrides/usr/share/wsl/wsl-oobe.sh
+++ runtime-data/wsl-default-settings/autobuild/overrides/usr/share/wsl/wsl-oobe.sh
@@ -20,10 +20,14 @@ while true; do
   # Create the user
   if /usr/bin/useradd --create-home --shell /bin/bash --uid "$DEFAULT_UID" --comment ''  "$username"; then
     if /usr/bin/usermod "$username" -aG "$DEFAULT_GROUPS"; then
-      /usr/bin/passwd "$username"
       break
     else
       /usr/bin/userdel --remove "$username"
     fi
   fi
+done
+
+while true; do
+  if /usr/bin/passwd "$username"; then
+    break
 done
