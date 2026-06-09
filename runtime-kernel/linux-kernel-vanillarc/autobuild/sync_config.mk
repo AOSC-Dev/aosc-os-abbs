08cf4bcfb9310cc0c996cd019656747c7af37e8f
Xi Ruoyao <xry111@xry111.site>
2026-06-09 20:28:35 +0800

    linux-kernel-vanillarc: update to 7.1.0-rc7
    - Sync kernel config from linux-kernel-7.0.12.

--- runtime-kernel/linux-kernel-vanillarc/autobuild/sync_config.mk
+++ runtime-kernel/linux-kernel-vanillarc/autobuild/sync_config.mk
@@ -4,9 +4,9 @@ OUTPUT_FILES=$(patsubst ../../linux-kernel-rc/autobuild/%,%,$(INPUT_FILES))
 all: $(OUTPUT_FILES)
 
 define OUTPUT_template =
-$(1): ../../linux-kernel-rc/autobuild/$(1) sync_config.mk
+$(1): ../../linux-kernel/autobuild/$(1) sync_config.mk
 	mkdir -pv $$(@D)
-	sed -r '/CONFIG_LOCALVERSION=/s/-rc(-|$$))/-vanillarc\1/' $$< > $$@
+	sed -r '/CONFIG_LOCALVERSION=/s/-(rc|main)(-|$$))/-vanillarc\2/' $$< > $$@
 endef
 
 $(foreach out,$(OUTPUT_FILES),$(eval $(call OUTPUT_template,$(out))))
