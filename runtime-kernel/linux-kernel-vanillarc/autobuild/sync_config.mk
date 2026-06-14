8df23fe1275451f6d404b4592290b4193def9284
Xi Ruoyao <xry111@xry111.site>
2026-06-14 22:08:02 +0800

    linux-kernel-vanillarc: disable CONFIG_MATH_EMULATION on ppc64el to fix FTBFS
    On ppc64el CONFIG_MATH_EMULATION is only for supporting some really old
    and bizzare hardware.  I guess it only belongs to Afterglow (if it's not
    simply unneeded at all).

--- runtime-kernel/linux-kernel-vanillarc/autobuild/sync_config.mk
+++ runtime-kernel/linux-kernel-vanillarc/autobuild/sync_config.mk
@@ -7,6 +7,7 @@ define OUTPUT_template =
 $(1): ../../linux-kernel/autobuild/$(1) sync_config.mk
 	mkdir -pv $$(@D)
 	sed -r '/CONFIG_LOCALVERSION=/s/-(rc|main)(-|$$))/-vanillarc\2/' $$< > $$@
+	case $(1) in *ppc64el* ) echo CONFIG_MATH_EMULATION=n >> $$@; esac
 endef
 
 $(foreach out,$(OUTPUT_FILES),$(eval $(call OUTPUT_template,$(out))))
